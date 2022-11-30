0d9f9f3f453d5df7090eaf0c99321fdc58fdcb7f
Alex Novak <alexleonnovak@gmail.com>
2022-11-30 15:23:31 +0200

    change flow save csv

--- src/services/csv.service.ts
+++ src/services/csv.service.ts
@@ -1,17 +1,9 @@
 import * as fs from 'fs';
+import {stringify} from 'csv-stringify/sync';
 
 export const saveCSV = (filename: string, data: Record<string, string>[]) => {
-  let isNewFile = false;
-  if (!fs.existsSync(filename)) {
-    isNewFile = true;
-  }
-  const stream = fs.createWriteStream(filename);
-  for (let i of data) {
-    if (isNewFile) {
-      stream.write(Object.keys(i).join(",") + "\r\n");
-      isNewFile = false;
-    }
-    stream.write(Object.values(i).join(",") + "\r\n");
-  }
-  stream.end();
+  const csv = stringify(data, {
+    header: !fs.existsSync(filename)
+  });
+  fs.appendFileSync(filename, csv);
 }
